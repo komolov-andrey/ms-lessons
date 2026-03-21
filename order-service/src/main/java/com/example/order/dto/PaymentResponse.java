5108f02a94da55e6410e2b37eaadcc9acdad6e32
Andrey Komolov <a.komolov@crestwavetech.ru>
2026-03-21 22:04:04 +0300

    add dto

---
+++ order-service/src/main/java/com/example/order/dto/PaymentResponse.java
@@ -0,0 +1,23 @@
+package com.example.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponse {
+    private UUID paymentId;
+    private String paymentNumber;
+    private String orderId;
+    private String status;
+    private String transactionId;
+    private LocalDateTime paymentDate;
+    private String errorMessage;
+}
